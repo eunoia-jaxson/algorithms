72574214cd542e87664bd3111f0cbacf45c681be
JinseoKim <eunoia@handong.ac.kr>
2024-05-16 10:10:09 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 212 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		long n = scanner.nextLong();
+		
+		System.out.println(n*(n-1)*(n-2)/6);
+		System.out.println(3);
+	}
+
+}
