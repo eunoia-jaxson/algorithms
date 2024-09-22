023da016e6277a3a3bbb55b81785afbf579d1138
JinseoKim <eunoia@handong.ac.kr>
2024-09-22 22:05:01 +0900

    [level 0] Title: 배열 만들기 4, Time: 23.29 ms, Memory: 38.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
@@ -0,0 +1,12 @@
+function solution(arr) {
+    var stk = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (stk.length === 0) stk.push(arr[i]);
+        else if (stk.at(-1) < arr[i]) stk.push(arr[i]);
+        else if (stk.at(-1) >= arr[i]) {
+            stk.pop();
+            i -= 1;
+        }
+    }
+    return stk;
+}
