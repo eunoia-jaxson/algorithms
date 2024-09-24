9afd9d1a8784ed25785bde0beff2c9c81ea80ac9
JinseoKim <eunoia@handong.ac.kr>
2024-09-24 15:10:32 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 66.68 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -5,7 +5,6 @@ function solution(participant, completion) {
         countMap[e] = (countMap[e] || 0) + 1;
     }
 
-    // arr1에서 차집합 구하기
     let result = [];
 
     for (let e of participant) {
@@ -14,9 +13,7 @@ function solution(participant, completion) {
             countMap[e]--;
         } else {
             // 제거할 값이 아니면 결과 배열에 추가
-            result.push(e);
+            return e;
         }
     }
-
-    return result[0];
 }
