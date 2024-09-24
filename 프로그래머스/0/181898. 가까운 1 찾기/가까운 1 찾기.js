76c8c0fa2c81576b25683c0edfdc8ed2ff14f958
JinseoKim <eunoia@handong.ac.kr>
2024-09-24 13:42:23 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.js
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -1,10 +1,3 @@
 function solution(arr, idx) {
-    let answer = -1;
-    for (const [i, e] of arr.entries()) {
-        if (i >= idx && e === 1) {
-            answer = i;
-            break;
-        }
-    }
-    return answer;
+    return arr.indexOf(1, idx);
 }
