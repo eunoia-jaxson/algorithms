9af7575d3a7a203922d447176be126e0eaf11b75
JinseoKim <eunoia@handong.ac.kr>
2024-09-30 20:00:02 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -1,4 +1,4 @@
 function solution(sides) {
-    const max = sides.sort((a, b) => a - b).pop();
-    return sides[0] + sides[1] <= max ? 2 : 1;
+    sides.sort((a, b) => a - b);
+    return sides[0] + sides[1] <= sides[2] ? 2 : 1;
 }
