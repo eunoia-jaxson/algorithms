14ab56248e2c8a5c4a3e636b369f3f6904eb5f2d
JinseoKim <eunoia@handong.ac.kr>
2024-10-01 20:17:01 +0900

    [level 0] Title: 0 떼기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.js
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.js
@@ -1,7 +1,3 @@
 function solution(n_str) {
-    for (const [i, e] of Array.from(n_str).entries()) {
-        if (e !== "0") {
-            return n_str.slice(i);
-        }
-    }
+    return Number(n_str).toString();
 }
