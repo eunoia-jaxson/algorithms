de3b0a2678aaf06d6acdcd9209d989e557b410c4
JinseoKim <eunoia@handong.ac.kr>
2024-10-02 10:08:41 +0900

    [level 0] Title: 날짜 비교하기, Time: 122.46 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/README.md
+++ 프로그래머스/0/181838. 날짜 비교하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 날짜 비교하기 - 181838 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181838) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181838#) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.05 ms
+메모리: 33.8 MB, 시간: 122.46 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 02일 10:07:33
+2024년 10월 02일 10:08:37
 
 ### 문제 설명
 
--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
@@ -1,9 +1,3 @@
 function solution(date1, date2) {
-    if (date1[0] < date2[0]) return 1;
-    else if (date1[0] > date2[0]) return 0;
-    else if (date1[1] < date2[1]) return 1;
-    else if (date1[1] > date2[1]) return 0;
-    else if (date1[2] < date2[2]) return 1;
-    else if (date1[2] > date2[2]) return 0;
-    else return 0;
+    return Number(new Date(date1) < new Date(date2));
 }
