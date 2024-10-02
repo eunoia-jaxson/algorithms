47b6a2de8926fb20c6cafd4e25bb415138f7bbd0
JinseoKim <eunoia@handong.ac.kr>
2024-10-02 09:11:22 +0900

    [level 0] Title: 숫자 찾기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.js
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.js
@@ -1,4 +1,3 @@
 function solution(num, k) {
-    const i = Array.from(num.toString()).indexOf(k.toString());
-    return i === -1 ? i : i + 1;
+    return Array.from(num.toString()).indexOf(k.toString()) + 1 || -1;
 }
