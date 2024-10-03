ad17ed61b6871edd2044a474e3d307d7ec959d3e
JinseoKim <eunoia@handong.ac.kr>
2024-10-03 20:03:08 +0900

    [level 0] Title: 7의 개수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120912. 7의 개수/7의 개수.js
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.js
@@ -1,3 +1,3 @@
 function solution(array) {
-    return array.flatMap(e => Array.from(e.toString())).filter(e => e === "7").length;
+    return Array.from(array.join("")).filter(e => e === "7").length;
 }
