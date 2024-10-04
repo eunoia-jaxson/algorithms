55eb681af61f482020e3ddac0fd3cf317d21090e
JinseoKim <eunoia@handong.ac.kr>
2024-10-04 10:05:17 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/README.md
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 최댓값 만들기 (2) - 120862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120862) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120862#) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.13 ms
+메모리: 33.5 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 04일 10:03:33
+2024년 10월 04일 10:05:13
 
 ### 문제 설명
 
--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,20 +1,4 @@
 function solution(numbers) {
-    if (numbers.length === 2) return numbers[0] * numbers[1];
-    const pos = numbers.filter(e => e >= 0);
-    const neg = numbers.filter(e => e < 0);
-    const answer = [];
-    
-    if (pos.length) {
-        const max = Math.max(...pos);
-        pos.splice(pos.indexOf(max), 1);
-        answer.push(max * (Math.max(...pos)));
-    }
-    
-    if (neg.length) {
-        const min = Math.min(...neg);
-        neg.splice(neg.indexOf(min), 1);
-        answer.push(min * (Math.min(...neg)));
-    }
-    
-    return Math.max(...answer);
+    numbers.sort((a, b) => a - b);
+    return Math.max(numbers[0] * numbers[1], numbers[numbers.length - 1] * numbers[numbers.length - 2]);
 }
