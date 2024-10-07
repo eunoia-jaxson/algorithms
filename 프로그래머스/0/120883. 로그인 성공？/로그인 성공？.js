14ee80f749f5db4b1f36849a3614503e097295ac
JinseoKim <eunoia@handong.ac.kr>
2024-10-07 10:56:04 +0900

    [level 0] Title: 로그인 성공?, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
@@ -2,7 +2,7 @@ function solution(id_pw, db) {
     const id = db.map(e => e[0]);
     const pw = db.map(e => e[1]);
     
-    if (id.indexOf(id_pw[0]) !== -1 && id_pw[1] === pw[id.indexOf(id_pw[0])]) return "login";
-    else if (id.indexOf(id_pw[0]) !== -1 && id_pw[1] !== pw[id.indexOf(id_pw[0])]) return "wrong pw";
+    if (id.includes(id_pw[0]) && id_pw[1] === pw[id.indexOf(id_pw[0])]) return "login";
+    else if (id.includes(id_pw[0]) && id_pw[1] !== pw[id.indexOf(id_pw[0])]) return "wrong pw";
     else return "fail";
 }
