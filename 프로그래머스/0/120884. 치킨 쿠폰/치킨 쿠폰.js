d220099dcc960a7d1d05029815237bc9d1154de1
JinseoKim <eunoia@handong.ac.kr>
2025-03-06 23:27:17 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.js
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -1,19 +1,10 @@
 function solution(chicken) {
     var answer = 0;
-    let remain = 0;
     
-    while (chicken) {
+    while (chicken >= 10) {
         answer += Math.floor(chicken / 10);
-        remain += chicken % 10;
-        chicken = Math.floor(chicken / 10);
+        chicken = Math.floor(chicken / 10) + (chicken % 10);
     }
-    
-    while (remain > 10) {
-        answer += Math.floor(remain / 10);
-        remain = Math.floor(remain / 10) + (remain % 10);
-    }
-    
-    if (remain === 10) answer += 1;
     
     return answer;
 }
