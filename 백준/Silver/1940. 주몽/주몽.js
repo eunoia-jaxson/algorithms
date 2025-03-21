66e41c39377caa1cc59462914343991d2ecd32c9
JinseoKim <eunoia@handong.ac.kr>
2025-03-21 09:23:46 +0900

    [Silver IV] Title: 주몽, Time: 320 ms, Memory: 11964 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.js
@@ -0,0 +1,23 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let input = [];
+
+readline.on('line', function(line) {
+    input.push(line.split(' ').map(el => parseInt(el)));
+}).on('close', function(){
+	const [N, M, ingredients] = input;
+    let answer = 0;
+    ingredients.sort((a, b) => b - a);
+    for (let i = 0; i < N - 1; i++) {
+        const current = ingredients.shift();
+        if (ingredients.includes(M - current)) {
+            answer += 1;
+        }
+    }
+    console.log(answer);
+    
+    process.exit();
+});
